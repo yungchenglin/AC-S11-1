25627d825c46c350cde97ecf857b92a438891cde
yungcheng <yungcheng111023@gmail.com>
2017-12-10 08:57:59 +0800

    add pg,update Gemfile.lock, change production db to pg

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,46 @@
+class PhotosController < ApplicationController
+    def index
+      @photos = Photo.all
+    end
+
+    def new
+      @photo = Photo.new
+    end
+
+    def create
+     @photo = Photo.new(photo_params)
+     @photo.save
+ 
+     redirect_to photos_url
+    end
+
+    def show
+      @photo = Photo.find(params[:id])
+    end
+
+    def edit
+      @photo = Photo.find(params[:id])
+    end
+      
+ 
+    def update
+      @photo = Photo.find(params[:id])
+      @photo.update_attributes(photo_params)
+
+      redirect_to photo_path(@photo)
+    end
+
+    def destroy
+      @photo = Photo.find(params[:id])
+      @photo.destroy
+
+      redirect_to photos_url
+    end
+
+ 
+    private
+
+    def photo_params
+       params.require(:photo).permit(:title, :date, :description, :file_location)
+    end
+end
